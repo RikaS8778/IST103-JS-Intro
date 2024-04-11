57279b21126ae5b65bc56495a9df4b05437827f6
Rika Sonohara <rika.s8778@gmail.com>
2024-04-10 20:43:06 -0700

    provided array data for activity3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //class10 activity
 //alert("Hello World");
+
+//activity2
 const macho = "Super ";
 const lucky_num = 777;
 const def_status = true;
@@ -13,6 +15,23 @@ for(let i = 0; i <=8; i++) {
     if (i=fav_num)
     console.log(fav_num + " is my favorit number!");
 }
+
+//activity3
+let todoList = [
+    "have dinner",
+    "transfer money from jpBank", 
+    "search MSP payment", 
+    "ask a question to my friend", 
+    "be opendminded"
+];
+
+let myProfile = {
+    fName: "Rika",
+    lName: "Sonohara",
+    hasChild: false,
+    age: 36
+};
+
    
 
 
