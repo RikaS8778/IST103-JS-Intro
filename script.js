b36ff8ab918d3a4af2d8cbebcd15e6419b3f27d9
Rika Sonohara <rika.s8778@gmail.com>
2024-04-10 19:56:40 -0700

    added for of loop in js file.

--- script.js
+++ script.js
@@ -3,10 +3,17 @@
 const macho = "Super ";
 const lucky_num = 777;
 const def_status = true;
+const fav_num = 8;
 
 if(def_status === true){
     console.log(macho + lucky_num + "!")
 };
+
+for(let i = 0; i <=8; i++) {
+    if (i=fav_num)
+    console.log(fav_num + " is my favorit number!");
+}
+   
 
 
 
