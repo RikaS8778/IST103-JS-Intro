71e10b160d51124dd082176251304f86e906c2cd
Rika Sonohara <rika.s8778@gmail.com>
2024-04-10 19:44:14 -0700

    added new lines in js files.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //class10 activity
-alert("Hello World");
+//alert("Hello World");
+const macho = "Super ";
+const lucky_num = 777;
+const def_status = true;
+
+if(def_status === true){
+    console.log(macho + lucky_num + "!")
+};
+
+
+
+
+
+
